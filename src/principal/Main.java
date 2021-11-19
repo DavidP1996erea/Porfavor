dde2d5a4762b6921d5fc062b7b6f45b6ed5ee7ea
David Perea García <david.perea@iesnervion.es>
2021-11-19 10:40:14 +0100

    vale

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -2,7 +2,9 @@ package principal;
 
 import java.util.Scanner;
 
+import static principal.digitio0y9.DIGITO;
 import static principal.multiplo5.multi;
+
 
 public class Main {
 
@@ -10,6 +12,7 @@ public class Main {
 	// write your code here
         Scanner sc= new Scanner(System.in);
 
+        /*
 
         System.out.println("¿Quieres añadir un año?");
         char opcion=sc.next().charAt(0);
@@ -24,6 +27,39 @@ public class Main {
              opcion=sc.next().charAt(0);
 
         }
+        */
+
+/*
+---------------------------------------------------------------------------------------------------------------
+ */
+
+
+
+        int digito=0;
+        char opc;
+        System.out.println("¿Quieres introducir un numero?");
+        opc=sc.next().charAt(0);
+
+        while(opc=='S' || opc=='s') {
+            System.out.println("Introduzca el numero");
+            digito = sc.nextInt();
+
+            DIGITO(digito);
+
+            System.out.println("¿Quieres volver a  introducir un numero?");
+            opc = sc.next().charAt(0);
+
+        }
+
+
+        /*
+---------------------------------------------------------------------------------------------------------------
+ */
+
+
+
+
+
 
 
     }
---
+++ src/principal/digitio0y9.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class digitio0y9 {
+
+    public static void DIGITO (int num){
+
+         if(num>=0 && num<=9){
+
+             System.out.println("Si está entre 0 y 9");
+
+         }else{
+
+             System.out.println("No está entre 0 y 9");
+         }
+
+
+    }
+}
