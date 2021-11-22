7b94a8704f57f7ce5ed4def9b7f1a9bbd1f43a4e
David Perea García <david.perea@iesnervion.es>
2021-11-22 13:57:15 +0100

    vale

--- src/principal/numperfecto.java
+++ src/principal/numperfecto.java
@@ -4,6 +4,19 @@ public class numperfecto {
 
     public static void numeroperfecto(int num){
 
+        int acum=0;
+
+        for(int i=1; i <= num ;i++){
+
+            if( num%i==0 ){
+
+                acum = acum +i;
+
+            }
+
+        }
+        System.out.println(acum);
+
 
 
     }
